43128291a5e718b4d38de3066147ae180d6041b4
coldenretriever <ejcaskie@buffalo.edu>
2026-02-13 19:27:05 -0500

    got html working! doing chat.html now!

--- util/host_path.py
+++ util/host_path.py
@@ -4,10 +4,29 @@ from util.response import Response
 def host_path(request, handler):
     res = Response()
     path = request.path
+
+
+    #INDEX.HTML
     if path == "/":
-        path = "public/index.html"
+        path = "./public/index.html"
+        with open("./public/layout/layout.html", "r", encoding="utf-8") as f:
+            layout = f.read()
+        with open(path, "r", encoding="utf-8") as f:
+            contents = f.read()
+
+        print(layout)
+        idx = layout.index("{{content}}")
+        layout = layout[0:idx] + contents + layout[idx:0]
+
+        #print("goooooooooooooooooooooooooooooal")
+        res.text(layout)
+        res.head["Content-Type"] = "text/html"
+        handler.request.sendall(res.to_data())
+
+
+    #CHAT.HTML
     elif path == "/chat":
-        path = "public/chat.html"
+        path = "./public/chat.html"
     try:
         print(path)
         with open("./" + path, "rb") as f:
