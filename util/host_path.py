a50ed992de0232e3916f6845fbcc2e4d6034b9f3
coldenretriever <ejcaskie@buffalo.edu>
2026-02-14 12:38:39 -0500

    gave router fix to make easier

--- util/host_path.py
+++ util/host_path.py
@@ -4,6 +4,35 @@ from util.response import Response
 def host_path(request, handler):
     res = Response()
     path = request.path
+    b = True
+
+    if path.__contains__(".js"):
+        res.head["Content-Type"] = "text/javascript"
+    elif path.__contains__(".jpg"):
+        res.head["Content-Type"] = "image/jpeg"
+    elif path.__contains__(".ico"):
+        res.head["Content-Type"] = "image/x-icon"
+    elif path.__contains__(".webp"):
+        res.head["Content-Type"] = "image/webp"
+    elif path.__contains__(".gif"):
+        res.head["Content-Type"] = "image/gif"
+    elif path.__contains__(".json"):
+        res.head["Content-Type"] = "application/json"
+    else:
+        b = False
+
+    if b:
+        try:
+            print(path)
+            with open("./" + path, "rb") as f:
+                file_bytes = f.read()
+            res.bytes(file_bytes)
+        except FileNotFoundError:
+            (
+                print("file not found"))
+
+        handler.request.sendall(res.to_data())
+
 
 
     #INDEX.HTML
@@ -43,28 +72,3 @@ def host_path(request, handler):
 
 
 
-    try:
-        print(path)
-        with open("./" + path, "rb") as f:
-            file_bytes = f.read()
-        res.bytes(file_bytes)
-    except FileNotFoundError:(
-        print("file not found"))
-
-    if path.__contains__("html"):
-        res.head["Content-Type"] = "text/html"
-    elif path.__contains__(".js"):
-        res.head["Content-Type"] = "text/javascript"
-    elif path.__contains__(".jpg"):
-        res.head["Content-Type"] = "image/jpeg"
-    elif path.__contains__(".ico"):
-        res.head["Content-Type"] = "image/x-icon"
-    elif path.__contains__(".webp"):
-        res.head["Content-Type"] = "image/webp"
-    elif path.__contains__(".gif"):
-        res.head["Content-Type"] = "image/gif"
-    elif path.__contains__(".json"):
-        res.head["Content-Type"] = "application/json"
-
-
-    handler.request.sendall(res.to_data())
--- util/router.py
+++ util/router.py
@@ -6,21 +6,26 @@ from util.response import Response
 class Router:
 
     def __init__(self):
-        self.routes = {}
+        self.methods = []
+        self.paths = []
+        self.actions = []
+        self.exact_paths = []
+        self.index = 0
 
     def add_route(self, method, path, action, exact_path=False):
-        self.routes[(method, path)] = {"action": action, "exact_path": exact_path}
-        #how do I want to store the paths so it can be compared?
+        self.methods.append(method)
+        self.paths.append(path)
+        self.actions.append(action)
+        self.exact_paths.append(exact_path)
+        self.index = self.index + 1
 
     def route_request(self, request, handler):
-
-        print("got through with: " + request.path)
-
-        for key in self.routes.keys():
-            if key[0] == request.method and (key[1] == request.path or (request.path.startswith(key[1]))):# and self.routes[key]["exact_path"] == False)):
-                print(request.path + "_________")
-                print("key: " + key[0] + " path " + key[1])
-                func = self.routes[key]["action"]
+        for i in range(self.index):
+            method = self.methods[i]
+            path = self.paths[i]
+            exact_path = self.exact_paths[i]
+            if method == request.method and (path == request.path or (request.path.startswith(path) and exact_path == False)):
+                func = self.actions[i]
                 func(request, handler)
                 return
 
